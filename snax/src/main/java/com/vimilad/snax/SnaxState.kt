6c2e1008a572387c67c05ed6ad7074194156746c
Milad Mohammadi <Mohammadi.dev@gmail.com>
2025-01-22 19:30:54 +0330

    Implemented Snackbar state

---
+++ snax/src/main/java/com/vimilad/snax/SnaxState.kt
@@ -0,0 +1,32 @@
+package com.vimilad.snax
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class SnaxState {
+
+    private val _data = mutableStateOf<SnaxData?>(null)
+    val data: State<SnaxData?> = _data
+
+    var updateState by mutableStateOf(false)
+        private set
+
+    fun setData(
+        type: SnaxType,
+        text: String,
+        title: String? = null,
+        actionTitle: String? = null,
+        action: (() -> Unit)? = null,
+    ) {
+        _data.value = SnaxData(
+            type = type,
+            text = text,
+            title = title,
+            actionTitle = actionTitle,
+            action = action
+        )
+        updateState = !updateState
+    }
+}
